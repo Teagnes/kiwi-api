ceab16c106a5b43b46eac1c8fe484d2e6996a941
kiwi <baos1020@163.com>
2023-09-24 00:00:45 +0800

    fix  更新sql语句

--- docker/db/win/init_data.sql
+++ docker/db/win/init_data.sql
@@ -1,4 +1,5 @@
 use kiwi;
+
 insert into  user (username,usercname,password) values
 ('u_admin','测试管理员','123456'),
 ('u_normal','测试用户','123456')
@@ -11,19 +12,14 @@ INSERT INTO role (name,description) values
 insert into user_role(user_id,role_id) values
 (1,1);
 
+-- 初始化管理员角色
 insert into permission (name, description )values
 ('p_user','用户管理权限'),
 ('p_role','角色管理权限'),
 ('p_permission','权限管理权限')
 ;
 
+-- 初始化管理员权限
 insert into role_permission (role_id,permission_id) values
 (1,1),(1,2),(1,3);
 
-;
-
-insert into  note ( create_user_id,note_name,note_uuid ) values
-(1,'测试文档库','xxxxxxxxxxxuuuuuuuuu');
-
-insert into  note_user (note_id,user_id) values
-(1,1);
